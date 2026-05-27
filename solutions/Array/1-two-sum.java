a1fc45d601f29d7bcfe1c44a061e98df4146ca6f
imking4all <121162349+imking4all@users.noreply.github.com>
2026-05-27 09:00:24 -0400

    Add solution: Two Sum

--- solutions/Array/1-two-sum.java
+++ solutions/Array/1-two-sum.java
@@ -1,15 +1,13 @@
-// Welcome to WorkPad
-// Start coding here...
-//Two Sum brute force method
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        for(int i = 0; i < nums.length; i++){
-            for(int j= i+1; j< nums.length; j++){
-                if(nums[i]+nums[j]==target){
-                    return new int[]{i, j};
-                }
+    public int removeDuplicates(int[] nums) {
+        int j = 0;
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] != nums[j]){
+                j++;
+                nums[j] = nums[i];          
             }
         }
-        return null;
+        return j+1;
+        
     }
 }
