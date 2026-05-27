c690ced787e6b00e1d9a88a4f6eef12365742a50
imking4all <121162349+imking4all@users.noreply.github.com>
2026-05-27 09:05:33 -0400

    Add solution: Two Sum

--- solutions/Array/1-two-sum.java
+++ solutions/Array/1-two-sum.java
@@ -1,13 +1,14 @@
 class Solution {
-    public int removeDuplicates(int[] nums) {
-        int j = 0;
-        for(int i = 1; i < nums.length; i++){
-            if(nums[i] != nums[j]){
-                j++;
-                nums[j] = nums[i];          
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(target-nums[i])){
+                return new int[]{map.get(target-nums[i]), i};
             }
+            map.put(nums[i], i);
         }
-        return j+1;
-        
+        return null;
     }
+
 }
