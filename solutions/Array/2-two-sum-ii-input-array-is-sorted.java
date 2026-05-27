951536f38c2db315333758cdf5968f1c8a53fa81
imking4all <121162349+imking4all@users.noreply.github.com>
2026-05-27 09:06:32 -0400

    Add solution: Two Sum II - Input Array Is Sorted

--- solutions/Array/2-two-sum-ii-input-array-is-sorted.java
+++ solutions/Array/2-two-sum-ii-input-array-is-sorted.java
@@ -1,13 +1,19 @@
 class Solution {
-    public int removeDuplicates(int[] nums) {
-        int j = 0;
-        for(int i = 1; i < nums.length; i++){
-            if(nums[i] != nums[j]){
-                j++;
-                nums[j] = nums[i];          
+    public int[] twoSum(int[] numbers, int target) {
+        int i = 0;
+        int j = numbers.length-1;
+        while(i<j){
+            int sum = numbers[i] + numbers[j];
+            if(sum == target){
+                return new int[]{i+1,j+1};
+            }
+            else if(sum > target){
+                j--;
+            }
+            else{
+                i++;
             }
         }
-        return j+1;
-        
+        return null;
     }
 }
